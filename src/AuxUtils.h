dc48e3664d28d568ea2a628dd7d147448de028e8
Slipperboy <380761489@qq.com>
2015-11-13 15:48:54 +0800

    2015.11.12更新
    实现了创建初始图像，高斯金字塔和高斯差分金子塔功能
    并添加了金字塔输出和显示辅助函数

---
+++ src/AuxUtils.h
@@ -0,0 +1,11 @@
+#ifndef AUXUTILS_H
+#define AUXUTILS_H
+#include <opencv2/opencv.hpp>
+#include <vector>
+
+void showPyr(const std::vector<cv::Mat> pyr,int nOctaves,int nOctaveLayers=3+3);
+
+void writePyr(const std::vector<cv::Mat> pyr,int nOctaves,int nOctaveLayers,
+	const char *dir,bool isStretch);
+
+#endif
---
+++ src/MySIFT.h
@@ -0,0 +1,69 @@
+#ifndef MYSIFT_H
+#define MYSIFT_H
+#include <opencv2/opencv.hpp>
+#include <vector>
+namespace mysift
+{
+// default number of sampled intervals per octave
+const int SIFT_INTVLS = 3;
+
+// default sigma for initial gaussian smoothing
+const float SIFT_SIGMA = 1.6f;
+
+// default threshold on keypoint contrast |D(x)|
+const float SIFT_CONTR_THR = 0.04f;
+
+// default threshold on keypoint ratio of principle curvatures
+const float SIFT_CURV_THR = 10.f;
+
+// double image size before pyramid construction?
+const bool SIFT_IMG_DBL = true;
+
+// default width of descriptor histogram array
+const int SIFT_DESCR_WIDTH = 4;
+
+// default number of bins per histogram in descriptor array
+const int SIFT_DESCR_HIST_BINS = 8;
+
+// assumed gaussian blur for input image
+const float SIFT_INIT_SIGMA = 0.5f;
+
+// width of border in which to ignore keypoints
+const int SIFT_IMG_BORDER = 5;
+
+// maximum steps of keypoint interpolation before failure
+const int SIFT_MAX_INTERP_STEPS = 5;
+
+// default number of bins in histogram for orientation assignment
+const int SIFT_ORI_HIST_BINS = 36;
+
+// determines gaussian sigma for orientation assignment
+const float SIFT_ORI_SIG_FCTR = 1.5f;
+
+// determines the radius of the region used in orientation assignment
+const float SIFT_ORI_RADIUS = 3 * SIFT_ORI_SIG_FCTR;
+
+// orientation magnitude relative to max that results in new feature
+const float SIFT_ORI_PEAK_RATIO = 0.8f;
+
+// determines the size of a single descriptor orientation histogram
+const float SIFT_DESCR_SCL_FCTR = 3.f;
+
+// threshold on magnitude of elements of descriptor vector
+const float SIFT_DESCR_MAG_THR = 0.2f;
+
+// factor used to convert floating-point descriptor to unsigned char
+const float SIFT_INT_DESCR_FCTR = 512.f;
+
+//创建金字塔的第一层图像
+void CreateInitialImage(const cv::Mat &src,cv::Mat &dst, bool doubleImageSize,double sigma=SIFT_SIGMA);
+
+void BuildGaussianPyramid( const cv::Mat& base, std::vector<cv::Mat>& pyr,
+	int nOctaves,int nOctaveLayers=3 ,double sigma=SIFT_SIGMA);
+
+void BuildDoGPyramid(std::vector<cv::Mat>& pyr,std::vector<cv::Mat>& dogpyr
+	,int nOctaves,int nOctaveLayers=3);
+}
+
+
+#endif
